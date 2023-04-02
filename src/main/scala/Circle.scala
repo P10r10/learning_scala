1f6ad3817c3cc932cd30df866252c7145ea911ea
P10R10 <asantiago@keemail.me>
2023-04-02 15:16:30 +0100

    circle

---
+++ src/main/scala/Circle.scala
@@ -0,0 +1,6 @@
+import scala.math.Pi
+import scala.math.pow
+
+object Circle {
+  def calculateArea(radius: Double): Double = Pi * pow(radius, 2.0)
+}
---
+++ src/main/scala/Tests.scala
@@ -0,0 +1,6 @@
+object Tests extends App {
+  println(Circle.calculateArea(1.0))
+  println(Circle.calculateArea(1.5))
+  println(Circle.calculateArea(2.0))
+
+}
